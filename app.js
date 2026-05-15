a52b8113f325faa0596b4c28c44966e805448d44
danidessssss <d.ledesma@alumnos.santotomas.cl>
2026-05-14 21:13:44 -0400

    update final, ya extrayendo datos en un json
    Ultimo commit de mi trabajo, ya extrae datos en un archivo json.

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
+const express = require('express');
+const scrapeRoutes = require('./routes/scrapeRoutes');
 
-const http = require('http');
+const app = express();
+const PORT = 3000;
 
+// procesamiento json
+app.use(express.json());
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; 
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('¡Hola Mundo!\n'); 
+app.get('/', (req, res) => {
+    res.send('<h1>Servidor de Scraping Activo</h1><p>Usa el endpoint: <a href="/api/scrape">/api/scrape</a> para ver los libros.</p>');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+app.use('/api', scrapeRoutes);
+
+
+app.use((req, res) => {
+    res.status(404).json({
+        status: 'error',
+        message: 'La ruta solicitada no existe.'// Manejo de rutas no encontradas (404)
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en: http://localhost:${PORT}`);
+    console.log(`Prueba el scraping en: http://localhost:${PORT}/api/scrape`);
 });
